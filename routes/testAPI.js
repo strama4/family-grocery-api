3140fc678230cd174c31f1422413e5e8ad62c62c
Luke Strama <strama@ualberta.ca>
2019-01-04 16:56:47 -0700

    test

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send("API is working properly");
+    res.send("API is finally fucking working properly");
 })
 
 module.exports = router;
