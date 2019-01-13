ffea832acddf08795c782db95ec24c083b4f8529
Luke Strama <strama@ualberta.ca>
2019-01-13 08:01:09 -0700

    added localhost to whitelist

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var listsRouter = require('./routes/lists');
 var app = express();
 
 require('./config/passport-config');
-var whitelist = ['https://laughing-wright-421c4b.netlify.com']
+var whitelist = ['https://laughing-wright-421c4b.netlify.com', 'https://localhost:3000']
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
